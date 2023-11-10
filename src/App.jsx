ceb54060146c1fb07e95c7d63700dd475d758623
mdsajalali <mdsajalali2020@gmail.com>
2023-11-10 15:05:47 +0600

    header section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Header from "./pages/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-center bg-slate-300">Donation Campaign</h1>
+    <div className="w-full min-h-screen  bg-cover bg-no-repeat bg-[linear-gradient(to_right_bottom,rgba(255,255,255,0.8),rgba(255,255,255,0.8)),url('./assets/images/bg-img.png')]">
+      <div className="container mx-auto">
+        <Header />
+      </div>
     </div>
   );
-}
+};
 
-export default App
-
-
+export default App;
---
+++ src/pages/Header.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/images/Logo.png";
+const Header = () => {
+  return (
+    <div className="flex flex-wrap items-center justify-center sm:justify-between gap-5 py-5 px-5">
+      <div>
+        <img className="w-32 md:w-44" src={logo} alt="logo" />
+      </div>
+      <nav>
+        <ul className="flex gap-10 text-[16px] md:text-[18px]">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Donation</a>
+          </li>
+          <li>
+            <a href="/">Statistics</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
